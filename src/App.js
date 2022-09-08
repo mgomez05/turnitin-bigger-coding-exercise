6ea42b08ba22eab3ca6e0c615e64e86fbb6cf2a5
mgomez05 <gomezruss7@gmail.com>
2022-09-08 00:46:12 -0400

    Wrote a simple GET request in componentDidMount() in App.js that logs the url retrieved for a random dog image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, {Component} from 'react'
 
 class App extends Component {
+
+
+  componentDidMount() {
+    const randomDogImageUrl = " https://dog.ceo/api/breeds/image/random"
+
+    fetch(randomDogImageUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("here's the url", data.message)
+      })
+      .catch((err) => {
+        console.log("There was an error getting the dog picture!")
+      })
+  }
+
   render() {
     return (
       <div className="RussApp">
