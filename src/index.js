2cf28d7c1126d9785097ef189c0c8fcb3bfa4918
mgomez05 <gomezruss7@gmail.com>
2022-09-08 00:32:05 -0400

    Replacted create-react-app boilerplate code in index.js with simpler code so it's easier to work with

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="RussApp">
+        <h1>Hello, Turnitin World!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
