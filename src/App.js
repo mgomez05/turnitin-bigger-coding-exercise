4bea497a53bb077c00a608e5692f00ed521f1142
mgomez05 <gomezruss7@gmail.com>
2022-09-08 22:08:57 -0400

    Added a h2 header claiming the first dog is your spirit dog, above the 1 random dog picture

--- src/App.js
+++ src/App.js
@@ -82,6 +82,7 @@ class App extends Component {
       <div className="RussApp">
         <h1>Hello, Turnitin World!</h1>
 
+        <h2>Here's Your Spirit Dog!</h2>
         <DogImage imageSourceUrl={randomDogImageUrl} />
 
         <h2>Here Are Your 10 Random Dogs!</h2>
