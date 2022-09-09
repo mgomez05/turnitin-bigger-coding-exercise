705d2242dc3c0cadf0d8dd0744c03a3de3122989
mgomez05 <gomezruss7@gmail.com>
2022-09-09 00:06:23 -0400

    Wrote really long function for converting the breed names from their url forms to their English equivalents

--- src/App.js
+++ src/App.js
@@ -71,7 +71,132 @@ class App extends Component {
     let breedAndImagePartOfUrl = url.split('https://images.dog.ceo/breeds/')[1]
     let breed = breedAndImagePartOfUrl.split('/')[0]
 
-    return breed 
+
+    let newBreedName = this.getPrettyBreedName(breed) 
+
+    console.log("new breed name: ", newBreedName)
+
+    return newBreedName
+  }
+
+  getPrettyBreedName(breedName) {
+
+    console.log("Getting breed ", breedName)
+
+    let breedNameArray = breedName.split('-')
+
+    if (breedNameArray.length === 1) {
+      let name = breedNameArray[0]
+      if (name === "mexicanhairless") return "Mexican Hairless"
+      else if (name === "stbernard") return "St Bernard"
+      else if (name === "germanshepherd") return "German Shepherd"
+      else if (name === "cotondetulear") return "Coton de Tulear"
+      else return this.firstLetterToUpperCase(breedNameArray[0])
+    }
+    else {
+
+      switch(breedNameArray[0]) {
+
+        case "australian": 
+          return "Australian Shepherd"
+
+        case "buhund":
+          return "Norwegian Buhund"
+
+        case "bulldog":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Bulldog"
+
+        case "bullterrier":
+          return "Staffordshire Bull Terrier"
+
+        case "cattledog":
+          return "Australian Cattle Dog"
+
+        case "collie":
+          return "Border Collie"
+
+        case "corgi":
+          return "Cardigan Corgi"
+
+        case "dane":
+          return "Great Dane"
+
+        case "deerhound":
+          return "Scottish Deerhound"
+
+        case "elkhound":
+          return "Norwegian Elkhound"
+
+        case "lapphund":
+          return "Finnish Lapphund"
+
+        case "frise":
+          return "Bichon Frisé"
+
+        case "greyhound":
+          return "Italian Greyhound"
+
+        case "hound":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Hound"
+
+        case "mastiff":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Mastiff"
+
+        case "mountain":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Mountain"
+
+        case "ovcharka":
+          return "Caucasian Ovcharka"
+
+        case "pinscher":
+          return "Miniature Pinscher"
+
+        case "pointer":
+          if (breedNameArray[1] === "german") return "German Pointer"
+          else return "German Longhaired Pointer"
+
+        case "poodle":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Poodle"
+
+        case "retriever":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Retriever"
+
+        case "ridgeback":
+          return "Rhodesian Ridgeback"
+
+        case "schnauzer":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Schnauzer"
+      
+        case "setter":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Setter"
+
+        case "sheepdog": 
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Sheepdog"
+
+        case "spaniel":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Spaniel"
+
+        case "springer":
+          return "English Springer"
+
+        case "terrier":
+          return this.firstLetterToUpperCase(breedNameArray[1]) + " Terrier"
+
+        case "waterdog":
+          return "Spanish Water Dog"
+
+        case "wolfhound":
+          return "Irish Wolfhound"
+        
+        default:
+          return this.firstLetterToUpperCase(breedNameArray[1]) + this.firstLetterToUpperCase(breedNameArray[0])
+      }
+
+    }
+  }
+
+  firstLetterToUpperCase(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   render() {
