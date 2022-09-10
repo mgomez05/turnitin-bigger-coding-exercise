4a08d1789797eb7a651c8cba1239fd1948dccbbc
mgomez05 <gomezruss7@gmail.com>
2022-09-09 20:07:18 -0400

    Fixed indentation and brackets in DogImageList.js

--- src/DogImageList.js
+++ src/DogImageList.js
@@ -6,28 +6,27 @@ const DogImageList = (props) => {
     const { dogImageObjects } = props
 
     if (dogImageObjects.length > 0) {
-    return (
-        <div className="galleryContainer">
-            { dogImageObjects.map((dogImageObject, index) => (
-                <div className="gallery" key={index}>
-                    <img src={dogImageObject.url} alt={"Couldn't Load Random Dog #" + index + "!"}></img>
-                    <h3>{getPrettyBreedName(dogImageObject.breed)}</h3>
-                </div>
-            ))}
-        </div>
-    )
-            }
-
-            else {
-                return (
-                    <div>
-                        <h3 id="tenDogsErrorMessage">{"Couldn't Load Your Random Dogs Pictures!"}</h3>
-                        <h4>Please try again later</h4>
+        return (
+            <div className="galleryContainer">
+                { dogImageObjects.map((dogImageObject, index) => (
+                    <div className="gallery" key={index}>
+                        <img src={dogImageObject.url} alt={"Couldn't Load Random Dog #" + index + "!"}></img>
+                        <h3>{getPrettyBreedName(dogImageObject.breed)}</h3>
                     </div>
+                ))}
+            </div>
+        )
+    }
 
-                )
-        
-            }
+    else {
+        return (
+            <div>
+                <h3 id="tenDogsErrorMessage">{"Couldn't Load Your Random Dogs Pictures!"}</h3>
+                <h4>Please try again later</h4>
+            </div>
+
+        )
+    }
 }
 
 export default DogImageList
